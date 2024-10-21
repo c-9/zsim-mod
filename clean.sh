f32bcc448971548cde206b6a1dcb87de80572240
chunk <chunk9@163.com>
2024-10-21 22:48:45 +0800

    dramsim merge

--- clean.sh
+++ clean.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-ZSIMPATH=$(pwd)
-PINPATH=$ZSIMPATH/lib/pin_2.14
-NVMAINPATH=$ZSIMPATH/lib/nvmain
-BOOST=$ZSIMPATH/lib/boost_1_59_0
-#LIBCONFIGPATH=$ZSIMPATH/lib/libconfig
+source ./env.sh
+
 NUMCPUS=$(grep -c ^processor /proc/cpuinfo)
 
 if [ "$1" = "all" ]
@@ -13,16 +10,10 @@ then
 	#cd $LIBCONFIGPATH
 	#make clean-libtool
 	#cd -
-	export PINPATH
-	export NVMAINPATH
-	export BOOST
 	scons -c
 	rm -rf build/ bin/
 else
 	echo "Cleaning Zsim ..."
-	export PINPATH
-	export NVMAINPATH
-	export BOOST
 	scons -c
 	rm -rf build/ bin/
 fi
--- compile.sh
+++ compile.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-ZSIMPATH=$(pwd)
-PINPATH=$ZSIMPATH/lib/pin_2.14
-NVMAINPATH=$ZSIMPATH/lib/nvmain
-BOOST=$ZSIMPATH/lib/boost_1_59_0
-#LIBCONFIGPATH=$ZSIMPATH/lib/libconfig
+source ./env.sh
 NUMCPUS=$(grep -c ^processor /proc/cpuinfo)
 
 if [ "$1" = "all" ]
@@ -20,15 +16,18 @@ then
         ./b2 --toolset=gcc-4.8 --buildtype=complete --cxxflags=" -g -std=c++0x -fabi-version=2 -D_GLIBCXX_USE_CXX11_ABI=0" -j $NUMCPUS
         ./b2 --toolset=gcc-4.8 --buildtype=complete --prefix=$BOOST install
         cd -
-
-        export PINPATH
-        export NVMAINPATH
-        export BOOST
         scons -j$NUMCPUS
+	mkdir bin
+	cd bin
+	ln -s ../build/opt/libzsim.so
+	ln -s ../build/opt/zsim
+	cd -
 else
 	echo "Compiling only ZSim ..."
-        export PINPATH
-        export NVMAINPATH
-        export BOOST
-        scons -j$NUMCPUS
+	scons -j$NUMCPUS
+	mkdir bin
+	cd bin
+	ln -s ../build/opt/libzsim.so
+        ln -s ../build/opt/zsim
+	cd -
 fi
